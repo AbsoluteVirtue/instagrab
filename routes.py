cdb25c84bc63c5d1002e98c55770b633c40f2809
Ivan <3hitemouse@gmail.com>
2020-04-21 01:30:05 +0300

    fixed url mismatch

--- routes.py
+++ routes.py
@@ -3,5 +3,5 @@ from controllers import home
 
 def setup_routes(app):
     app.router.add_view(r'/', handler=home.Main, name='home')
-    app.router.add_view(r'/{name:\S+}', handler=home.Main, name='main')
-    app.router.add_view(r'/{id:\S+}', handler=home.SinglePost, name='single')
+    app.router.add_view(r'/{name:^[A-Za-z0-9-_]+$}', handler=home.Main, name='main')
+    app.router.add_view(r'/p/{id:\S+}', handler=home.SinglePost, name='single')
